feaf5e026263e701b59cee488bc50ad3c89f22e7
HaoranYueRyan <92607722+HaoranYueRyan@users.noreply.github.com>
2023-03-28 21:15:47 +0100

    updata the meathod of throushold

--- main.py
+++ main.py
@@ -1,31 +1,45 @@
 import os
 import matplotlib
 matplotlib.use('TkAgg')
-from measure_area import load_image,threshold_image,measure_area
+from measure_area import load_image,threshold_image,measure_area,denoise_image
 import natsort
 import matplotlib.pyplot as plt
 import pandas as pd
-
-
-folder_path = './astral_microtubules/'
-
+import cv2
+import numpy as np
+from skimage import  filters
+# folder_path = './astral_microtubules/'
+folder_path = './images2/'
 image_extensions = ('.tif', '.png', '.jpg', '.jpeg', '.bmp')
 
-image_names = ['./astral_microtubules/'+filename for filename in os.listdir(folder_path) if filename.lower().endswith(image_extensions)]
+image_names = [folder_path+filename for filename in os.listdir(folder_path) if filename.lower().endswith(image_extensions)]
 image_names= natsort.natsorted(image_names)
 
-def main(images_names:list,save_name)->pd.DataFrame:
-    area_list=[]
-    for img in images_names:
-        image_gray = load_image(img)
-        # cv2.imwrite('gray_image_{0}_{1}.png', image_gray.astype(np.uint8) * 255)
-        binary_image = threshold_image(image_gray)
+def main(image_paths,save_name):
+    area_list = []
+
+    first_frame = True
+    fixed_threshold = 0
+
+    for image_path in image_paths:
+        image_gray = load_image(image_path)
+
+        if first_frame:
+            fixed_threshold = filters.threshold_otsu(image_gray)*1.15
+            first_frame = False
+        else:
+            current_threshold = filters.threshold_otsu(image_gray)
+            if current_threshold > fixed_threshold:
+                fixed_threshold = current_threshold
+
+        binary_image = image_gray > fixed_threshold
+        denoised_image = denoise_image(binary_image)
+        cv2.imwrite(f"./output/{os.path.basename(image_path)}", (denoised_image.astype(np.uint8) * 255))
         centre_area, total_area = measure_area(binary_image)
         area_list.append(centre_area)
-        print(f"The centre area of astral microtubules is: {centre_area} pixels")
 
-    df=pd.DataFrame(area_list)
-    df.columns=['area']
+    df = pd.DataFrame(area_list)
+    df.columns = ['area']
     df.to_csv(f"./{save_name}.csv")
     # Create a line plot
     plt.plot(area_list)
@@ -35,6 +49,10 @@ def main(images_names:list,save_name)->pd.DataFrame:
     plt.title('area of astral microtubules ')
     # Display the plot
     plt.show()
+
+    return area_list
+
+
 
 
 
--- measure_area.py
+++ measure_area.py
@@ -1,6 +1,6 @@
 import cv2
 import numpy as np
-from skimage import io, filters, measure
+from skimage import io, filters, measure,morphology
 import matplotlib.pyplot as plt
 
 # Load and preprocess the image
@@ -11,8 +11,15 @@ def load_image(image_url):
 # Threshold the image to detect astral microtubules
 def threshold_image(image_gray):
     threshold_value = filters.threshold_otsu(image_gray)
-    binary_image = image_gray > threshold_value
+    binary_image = image_gray > threshold_value*1.15
     return binary_image
+
+def denoise_image(binary_image):
+    selem = morphology.disk(3) # Use disk-shaped structuring element
+    denoised_img = morphology.opening(binary_image, selem)
+    return denoised_img
+
+
 
 # Measure the total area of the microtubules
 def measure_area(binary_image):
@@ -25,10 +32,11 @@ def measure_area(binary_image):
     return centre_area,total_area
 
 if __name__ == "__main__":
-    image_url = "./images/exp2022_H1299_EB3-mKate2_SiR-DNA_pi-EB1-GFP_set11_STLC_CilioDi_cell1_R3D_D3D_t149_c002.tif"
+    image_url = "./images/exp2022_H1299_EB3-mKate2_SiR-DNA_pi-EB1-GFP_set11_STLC_CilioDi_cell1_R3D_D3D_t149_c001.tif"
     image_gray = load_image(image_url)
-    # cv2.imwrite('gray_image_{0}_{1}.png', image_gray.astype(np.uint8) * 255)
+    cv2.imwrite('gray_image_{0}_{1}.png', image_gray.astype(np.uint8) * 255)
     binary_image = threshold_image(image_gray)
+    cv2.imwrite('binary_image_{0}_{1}.png',binary_image.astype(np.uint8) * 255)
     centre_area,total_area = measure_area(binary_image)
     print(f"The centre area of astral microtubules is: {centre_area} pixels\n "  
           f"The total area of astral microtubules is: {total_area} pixels")
